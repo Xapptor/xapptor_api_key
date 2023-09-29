9e2a6be47165a5593959cf28cbca89f8bfc1ba3e
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2023-09-29 04:25:38 -0600

    Feat: dependencies updated

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/javiergarcia/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/javiergarcia/Documents/Xtr/xapptor_modules/xapptor_api_key"
+export "FLUTTER_APPLICATION_PATH=/Users/javiergarcia/Documents/xtrdcs/xtr/modules/xapptor_api_key"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=0.0.1"
