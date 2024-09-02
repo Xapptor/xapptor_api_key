056a868675e94cb512a5f36b501ebded6ca12c2b
Javier Garcia <javierjesusgarciacontreras@gmail.com>
2024-09-02 14:58:48 -0600

    Feat: xapptor_db added

--- lib/gak.dart
+++ lib/gak.dart
@@ -1,12 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:universal_platform/universal_platform.dart';
 import 'package:xapptor_api_key/initial_values.dart';
+import 'package:xapptor_db/xapptor_db.dart';
 
 Future<String> gak({
   required String n,
   required String o,
 }) async {
-  DocumentSnapshot o_m = await FirebaseFirestore.instance.collection("metadata").doc(o).get();
+  DocumentSnapshot o_m = await XapptorDB.instance.collection("metadata").doc(o).get();
   Map<String, dynamic> o_d = o_m.data() as Map<String, dynamic>;
   String ak = "";
   String p_n = UniversalPlatform.operatingSystem.toString();
